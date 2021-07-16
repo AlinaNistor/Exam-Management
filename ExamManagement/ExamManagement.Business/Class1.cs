dcc310f920b0d2da6b21a9f2a237b4a93fd07db7
adrianteohari <73904025+adrianteohari@users.noreply.github.com>
2021-07-16 15:17:32 +0300

    changed the names inside the project with the project title

---
+++ ExamManagement/ExamManagement.Business/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ExamManagement.Business
+{
+    public class Class1
+    {
+    }
+}
---
+++ ExamManagement/ExamManagement.Persistence/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ExamManagement.Persistence
+{
+    public class Class1
+    {
+    }
+}
