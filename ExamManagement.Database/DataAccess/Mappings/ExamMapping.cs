52702973669e6aeab87f9e89148e0e73c86a1e7a
RazvanIonut2599 <73965890+RazvanIonut2599@users.noreply.github.com>
2021-07-22 16:56:21 +0300

    DatabaseLayerAdded
    Added Mappings
    Added Entities
    Added InitialMigration

---
+++ ExamManagement.Database/DataAccess/Mappings/ExamMapping.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Mappings
+{
+    internal abstract class ExamMapping
+    {
+        internal static void Map(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Exam>()
+                            .Property(s => s.Id)
+                            .HasColumnName("Id")
+                            .IsRequired();
+
+            modelBuilder.Entity<Exam>()
+                            .Property(s => s.Faculty)
+                            .HasColumnName("Faculty")
+                            .IsRequired();
+
+            modelBuilder.Entity<Exam>()
+                            .Property(s => s.YearOfStudy)
+                            .HasColumnName("YearOfStudy")
+                            .IsRequired();
+
+            modelBuilder.Entity<Exam>()
+                            .Property(s => s.Mandatory)
+                            .HasColumnName("Mandatory")
+                            .IsRequired();
+
+            modelBuilder.Entity<Exam>()
+                            .Property(s => s.Name)
+                            .HasColumnName("Name")
+                            .HasMaxLength(30)
+                            .IsRequired();
+
+            modelBuilder.Entity<Exam>()
+                            .Property(s => s.HeadProfessor)
+                            .HasColumnName("HeadProfessor")
+                            .HasMaxLength(30)
+                            .IsRequired();
+
+            modelBuilder.Entity<Exam>()
+                            .Property(s => s.Date)
+                            .HasColumnName("Date");
+
+
+            modelBuilder.Entity<Exam>()
+                            .Property(s => s.ExamType)
+                            .HasColumnName("ExamType");
+                            
+
+            modelBuilder.Entity<Exam>()
+                            .Property(s => s.Location)
+                            .HasColumnName("Location")
+                            .HasMaxLength(50);
+            modelBuilder.Entity<Exam>()
+                .Property(s => s.DateAdded)
+                .HasColumnName("DateAdded")
+                .HasMaxLength(20)
+                .IsRequired();
+
+
+            modelBuilder.Entity<Exam>()
+                .HasMany(s => s.Questions)
+                .WithOne(g => g.Exam)
+                .HasForeignKey(s => s.ExamId)
+                .OnDelete(DeleteBehavior.SetNull);
+
+            modelBuilder.Entity<Exam>()
+               .HasMany(s => s.Attendances)
+               .WithOne(g => g.Exam)
+               .HasForeignKey(s => s.ExamId)
+               .OnDelete(DeleteBehavior.SetNull);
+
+
+        }
+    }
+}
---
+++ ExamManagement.Database/DataAccess/Mappings/UserMapping.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Mappings
+{
+    internal abstract class UserMapping
+    {
+        internal static void Map(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .Property(s => s.LastName)
+                .HasColumnName("LastName")
+                .HasMaxLength(20)
+                .IsRequired();
+            modelBuilder.Entity<User>()
+               .Property(s => s.FirstName)
+               .HasColumnName("FirstName")
+               .HasMaxLength(50)
+               .IsRequired();
+            modelBuilder.Entity<User>()
+                  .Property(s => s.Email)
+                  .HasColumnName("Email")
+                  .HasMaxLength(50)
+                  .IsRequired();
+            modelBuilder.Entity<User>()
+                      .Property(s => s.Password)
+                      .HasColumnName("Password")
+                      .HasMaxLength(250)
+                      .IsRequired();
+            modelBuilder.Entity<User>()
+                  .Property(s => s.Role)
+                  .HasColumnName("Role")
+                  .IsRequired();
+            modelBuilder.Entity<User>()
+                      .Property(s => s.YearOfStudy)
+                      .HasColumnName("YearOfStudy");
+            modelBuilder.Entity<User>()
+                  .Property(s => s.Tax)
+                  .HasColumnName("Tax");
+
+
+            modelBuilder.Entity<User>()
+                .HasMany(s => s.Attendances)
+                .WithOne(g => g.Student)
+                .HasForeignKey(s => s.StudentId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<User>()
+                .HasMany(s => s.Questions)
+                .WithOne(g => g.User)
+                .HasForeignKey(s => s.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<User>()
+                .HasMany(s => s.Answers)
+                .WithOne(g => g.User)
+                .HasForeignKey(s => s.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+    }
+    }
+}
