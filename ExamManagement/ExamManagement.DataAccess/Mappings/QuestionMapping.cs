7acdd0087cc93dbd4bc641a4fc7fb9540c675353
RazvanIonut2599 <73965890+RazvanIonut2599@users.noreply.github.com>
2021-07-22 18:36:55 +0300

    Solved the problems
    am mutat folderele cu totu , before it just made a reference. lets see if this solves stuff

---
+++ ExamManagement/ExamManagement.DataAccess/Mappings/QuestionMapping.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Mappings
+{
+    internal abstract class QuestionMapping
+    {
+        internal static void Map(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Question>()
+                           .Property(s => s.Id)
+                           .HasColumnName("Id")
+                           .IsRequired();
+
+            //modelBuilder.Entity<Question>()
+            //               .Property(s => s.ExamId)
+            //               .HasColumnName("ExamId")
+            //               ;
+            
+            //modelBuilder.Entity<Question>()
+            //               .Property(s => s.UserId)
+            //               .HasColumnName("UserId")
+            //               .IsRequired();
+
+            modelBuilder.Entity<Question>()
+                           .Property(s => s.Text)
+                           .HasColumnName("Text")
+                           .HasMaxLength(255)
+                           .IsRequired();
+
+            modelBuilder.Entity<Question>()
+                .Property(s => s.DateAdded)
+                .HasColumnName("DateAdded")
+                .HasMaxLength(20)
+                .IsRequired();
+
+            modelBuilder.Entity<Question>()
+                .HasMany(s => s.Answers)
+                .WithOne(g => g.Question)
+                .HasForeignKey(s => s.QuestionId)
+                .OnDelete(DeleteBehavior.NoAction);
+         
+    }
+    }
+}
---
+++ ExamManagement/ExamManagement.Entities/Answer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Answer:BaseEntity
+    {
+        public Guid? QuestionId { get; set; }
+        public Guid UserId { get; set; }
+        public string Text { get; set; }
+
+        public string DateAdded { get; set; }
+
+        public virtual Question Question { get; set; }
+        public virtual User User { get; set; }
+    }
+}
