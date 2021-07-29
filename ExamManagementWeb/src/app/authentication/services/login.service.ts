978b4fdf57db6005bb5d283eacee95ecdb4e9da9
Alina Nistor <alina.nistor99@gmail.com>
2021-07-29 19:49:54 +0300

    Update services

--- ExamManagementWeb/src/app/authentication/services/login.service.ts
+++ ExamManagementWeb/src/app/authentication/services/login.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { LoginModel } from '../models/login.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class LoginService {
+export class AuthenticationService {
+  constructor(private readonly httpClient: HttpClient) {}
+  private url: string = 'https://localhoste:5000/authetication/login';
 
-  constructor() { }
+  public login(data: LoginModel): Observable<HttpResponse<unknown>> {
+    return this.httpClient.post(this.url, data, { observe: 'response' });
+  }
 }
--- ExamManagementWeb/src/app/authentication/services/register.service.ts
+++ ExamManagementWeb/src/app/authentication/services/register.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { RegisterModel } from '../models/register.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class RegisterService {
+export class AuthenticationService {
+  constructor(private readonly httpClient: HttpClient) {}
+  private url: string = 'https://localhoste:5000/authetication/register';
 
-  constructor() { }
+  public register(data: RegisterModel): Observable<HttpResponse<any>> {
+    return this.httpClient.post<HttpResponse<any>>(this.url, data, {
+      observe: 'response',
+    });
+  }
 }
