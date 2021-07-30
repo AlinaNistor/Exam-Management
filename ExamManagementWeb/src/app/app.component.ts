dcab2e454948e6e23780ff86a731332188d5bc0c
Alina Nistor <alina.nistor99@gmail.com>
2021-07-31 00:36:13 +0300

    Update app.component

--- ExamManagementWeb/src/app/app.component.ts
+++ ExamManagementWeb/src/app/app.component.ts
@@ -1,18 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/shared/services/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-
-export class AppComponent {
+export class AppComponent implements OnInit {
+  public username: string = '';
   title = 'Exam management';
-
   isLogin = true;
   sideBarOpen = true;
 
+  constructor(public readonly userService: UserService) {}
+  ngOnInit(): void {
+    this.getUserName();
+  }
+
   sideBarToggler() {
     this.sideBarOpen = !this.sideBarOpen;
+  }
+
+  public getUserName(): void {
+    if (sessionStorage.getItem('identity') != null)
+      this.userService.username = JSON.parse(
+        sessionStorage.getItem('identity')!.toString()
+      )['email'];
+    this.username = this.userService.username;
   }
 }
