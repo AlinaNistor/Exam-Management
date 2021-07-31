8c8b6530f206a3b7eb321d391b6cadc6634ee30f
Alina Nistor <alina.nistor99@gmail.com>
2021-08-01 02:44:09 +0300

    Update exam service

--- ExamManagementWeb/src/app/find-exam/find-exam.component.ts
+++ ExamManagementWeb/src/app/find-exam/find-exam.component.ts
@@ -1,53 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ExamModel } from '../shared/models/exam.model';
+import { ExamService } from '../shared/services/exam.service';
+import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-find-exam',
   templateUrl: './find-exam.component.html',
-  styleUrls: ['./find-exam.component.scss']
+  styleUrls: ['./find-exam.component.scss'],
 })
-export class FindExamComponent implements OnInit {
+export class FindExamComponent implements OnInit, OnDestroy {
+  public examsList: ExamModel[] = new Array<ExamModel>();
+  private subs: Subscription[];
 
-  public examsList = [
-    {
-      title: 'Title1',
-      prof: 'Prof1',
-      date: 'Date1',
-      description:
-        'Description1 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam velit nisi, tristique sed erat ac,scelerisque tempor metus. Curabitur mi velit, pharetra in lacinia tristique, commodo ac enim. Integer rutrum varius elit ut ultricies. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
-      link: 'link1',
-    },
-
-    {
-      title: 'Title2',
-      prof: 'Prof2',
-      date: 'Date2',
-      description:
-        'Description2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam velit nisi, tristique sed erat ac,scelerisque tempor metus. Curabitur mi velit, pharetra in lacinia tristique, commodo ac enim. Integer rutrum varius elit ut ultricies. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
-      link: 'link2',
-    },
-
-    {
-      title: 'Title3',
-      prof: 'Prof3',
-      date: 'Date3',
-      description:
-        'Description3 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam velit nisi, tristique sed erat ac,scelerisque tempor metus. Curabitur mi velit, pharetra in lacinia tristique, commodo ac enim. Integer rutrum varius elit ut ultricies. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
-      link: 'link3',
-    },
-
-    {
-      title: 'Title4',
-      prof: 'Prof4',
-      date: 'Date4',
-      description:
-        'Description1 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam velit nisi, tristique sed erat ac,scelerisque tempor metus. Curabitur mi velit, pharetra in lacinia tristique, commodo ac enim. Integer rutrum varius elit ut ultricies. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
-      link: 'link4',
-    },
-  ];
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private readonly examService: ExamService) {
+    this.subs = new Array<Subscription>();
   }
 
+  ngOnInit(): void {
+    this.subs.push(
+      this.examService.getAllExams().subscribe((data: HttpResponse<any>) => {
+        this.examsList = data.body;
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subs.forEach((sub) => {
+      sub.unsubscribe();
+    });
+  }
 }
