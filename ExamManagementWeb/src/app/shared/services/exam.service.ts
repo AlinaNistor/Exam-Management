4a373a1f82d56582a07dba9e8679b4e05dc1f2a7
Alina Nistor <alina.nistor99@gmail.com>
2021-08-01 02:20:19 +0300

    Add exam service

---
+++ ExamManagementWeb/src/app/shared/services/exam.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ExamService {
+  private url: string = 'https://localhost:5000/api/Exam';
+  constructor(private readonly httpClient: HttpClient) {}
+
+  public getAllExams(): Observable<HttpResponse<unknown>> {
+    return this.httpClient.get<HttpResponse<unknown>>(this.url, {
+      observe: 'response',
+    });
+  }
+}
