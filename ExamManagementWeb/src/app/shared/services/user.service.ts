0589b18e098fb828e989f125e62b51443d1d1d7d
Alina Nistor <alina.nistor99@gmail.com>
2021-07-31 21:22:03 +0300

    Update user.service.ts

--- ExamManagementWeb/src/app/shared/services/user.service.ts
+++ ExamManagementWeb/src/app/shared/services/user.service.ts
@@ -4,7 +4,8 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class UserService {
-  public username: string = '';
-
+  public firstName: string = '';
+  public lastName: string = '';
+  public email: string = '';
   constructor() {}
 }
