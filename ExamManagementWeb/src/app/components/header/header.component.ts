e45caaa455cebc8b871be58d31849d81f1745eee
Alina Nistor <alina.nistor99@gmail.com>
2021-08-01 01:33:45 +0300

    Add first name and last name in header

--- ExamManagementWeb/src/app/components/header/header.component.ts
+++ ExamManagementWeb/src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnInit, Output, Input } from '@angular/core';
 import { Router } from '@angular/router';
+import { UserService } from 'src/app/shared/services/user.service';
 
 @Component({
   selector: 'app-header',
@@ -8,10 +9,22 @@ import { Router } from '@angular/router';
 })
 export class HeaderComponent implements OnInit {
   @Output() toggleSidebarForMe: EventEmitter<any> = new EventEmitter();
+  public firstName: string = '';
+  public lastName: string = '';
+  public user: string = '';
 
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    public readonly userService: UserService
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (sessionStorage.getItem('identity') != null) {
+      this.firstName =  titleCaseWord(JSON.parse(sessionStorage.getItem('identity')!)['firstName']);
+      this.lastName =  titleCaseWord(JSON.parse(sessionStorage.getItem('identity')!)['lastName']);
+      this.user =  titleCaseWord(JSON.parse(sessionStorage.getItem('identity')!)['email']);
+    }
+  }
 
   toggleSidebar() {
     this.toggleSidebarForMe.emit();
@@ -21,4 +34,9 @@ export class HeaderComponent implements OnInit {
     sessionStorage.clear();
     this.router.navigate(['authentication']);
   }
+}
+
+function titleCaseWord(word: string) {
+  if (!word) return word;
+  return word[0].toUpperCase() + word.substr(1).toLowerCase();
 }
