d84f5093bad67a415ba9e401227f0000c7ebf31a
Alina Nistor <alina.nistor99@gmail.com>
2021-08-01 02:17:14 +0300

    Create exam.model.ts

---
+++ ExamManagementWeb/src/app/shared/models/exam.model.ts
@@ -0,0 +1,12 @@
+export type ExamModel ={
+  id:string;
+  faculty:string;
+  yearOfStudy: number,
+  mandatory: number,
+  name: string,
+  headProfessor: string,
+  date: string,
+  examType: number,
+  location: string
+};
+
