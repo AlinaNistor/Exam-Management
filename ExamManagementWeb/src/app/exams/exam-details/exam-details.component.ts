1746a663504825bf79554c0da63eae7f9a9ad036
Alina Nistor <alina.nistor99@gmail.com>
2021-08-01 21:28:00 +0300

    Add exam-details component

---
+++ ExamManagementWeb/src/app/exams/exam-details/exam-details.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
+import { HttpResponse, HttpClient } from '@angular/common/http';
+import  jwt_decode from 'jwt-decode';
+import { Subscription } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { ExamService } from 'src/app/shared/services/exam.service';
+
+@Component({
+  selector: 'app-exam-details',
+  templateUrl: './exam-details.component.html',
+  styleUrls: ['./exam-details.component.scss'],
+})
+export class ExamDetailsComponent implements OnInit, OnDestroy {
+  private userId: string = '';
+  private routeSub: Subscription = new Subscription();
+  private exam: any;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private activatedRoute: ActivatedRoute,
+    private examService: ExamService
+  ) {}
+
+  ngOnDestroy(): void {
+    this.routeSub.unsubscribe();
+  }
+
+  ngOnInit(): void {
+    this.routeSub = this.activatedRoute.params.subscribe((params) => {
+      this.examService
+        .getExam(params['id'])
+        .subscribe((data: HttpResponse<any>) => {
+          this.exam= data.body;
+          console.log(this.exam);
+        });
+    });
+
+    //this.userId = jwt_decode(sessionStorage.getItem('userToken')!).userId;
+  }
+}
