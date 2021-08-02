643df72cfda8c870ff7a00327dbda8431863a2e5
Alina Nistor <alina.nistor99@gmail.com>
2021-08-02 13:27:06 +0300

    Update exam card

--- ExamManagementWeb/src/app/find-exam/find-exam.component.ts
+++ ExamManagementWeb/src/app/find-exam/find-exam.component.ts
@@ -4,6 +4,8 @@ import { ExamService } from '../shared/services/exam.service';
 import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
 import { Subscription } from 'rxjs';
 import { Router } from '@angular/router';
+import { FacultyService } from 'src/app/shared/services/faculty.service';
+import { FacultyModel } from 'src/app/shared/models/faculty.model';
 
 @Component({
   selector: 'app-find-exam',
@@ -12,9 +14,11 @@ import { Router } from '@angular/router';
 })
 export class FindExamComponent implements OnInit, OnDestroy {
   public examsList: ExamModel[] = new Array<ExamModel>();
+  public faculties: FacultyModel[] = new Array<FacultyModel>();
   private subs: Subscription[];
 
   constructor(private readonly examService: ExamService,
+    private facultyService: FacultyService,
     private router: Router) {
     this.subs = new Array<Subscription>();
   }
@@ -23,6 +27,12 @@ export class FindExamComponent implements OnInit, OnDestroy {
     this.subs.push(
       this.examService.getAllExams().subscribe((data: HttpResponse<any>) => {
         this.examsList = data.body;
+
+        this.facultyService
+        .getFaculties()
+        .subscribe((faculties: HttpResponse<any>) => {
+          this.faculties = faculties.body;
+        });
       })
     );
   }
@@ -31,6 +41,10 @@ export class FindExamComponent implements OnInit, OnDestroy {
     this.subs.forEach((sub) => {
       sub.unsubscribe();
     });
+  }
+
+  public getFacultyName(facultyId: string){
+    return this.faculties.find((c:FacultyModel) => c.id == facultyId)?.name;
   }
 
   goToExam(id: string): void {
