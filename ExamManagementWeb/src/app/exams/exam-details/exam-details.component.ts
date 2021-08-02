4ca4457bf01561fbfcba2c45c709269dc169e193
Alina Nistor <alina.nistor99@gmail.com>
2021-08-02 03:20:12 +0300

    Update exam content

--- ExamManagementWeb/src/app/exams/exam-details/exam-details.component.ts
+++ ExamManagementWeb/src/app/exams/exam-details/exam-details.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
 import { HttpResponse, HttpClient } from '@angular/common/http';
-import  jwt_decode from 'jwt-decode';
+import jwt_decode from 'jwt-decode';
 import { Subscription } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { ExamService } from 'src/app/shared/services/exam.service';
 import { ExamModel } from 'src/app/shared/models/exam.model';
+import { FacultyService } from 'src/app/shared/services/faculty.service';
+import { FacultyModel } from 'src/app/shared/models/faculty.model';
 
 @Component({
   selector: 'app-exam-details',
@@ -16,11 +18,13 @@ export class ExamDetailsComponent implements OnInit, OnDestroy {
   private userId: string = '';
   private routeSub: Subscription = new Subscription();
   public exam!: ExamModel;
+  public faculty!: FacultyModel;
 
   constructor(
     private formBuilder: FormBuilder,
     private activatedRoute: ActivatedRoute,
-    private examService: ExamService
+    private examService: ExamService,
+    private facultyService: FacultyService
   ) {}
 
   ngOnDestroy(): void {
@@ -32,8 +36,14 @@ export class ExamDetailsComponent implements OnInit, OnDestroy {
       this.examService
         .getExam(params['id'])
         .subscribe((data: HttpResponse<any>) => {
-          this.exam= data.body;
-          console.log(this.exam);
+          this.exam = data.body;
+
+          this.facultyService
+            .getFaculties()
+            .subscribe((faculties: HttpResponse<any>) => {
+              this.faculty = faculties.body.find((c:FacultyModel) => c.id == this.exam.facultyId).name;
+            });
+
         });
     });
 
