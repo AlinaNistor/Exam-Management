6222e58e5a0a05852497c33c0b35f790b2f8ff61
Alina Nistor <alina.nistor99@gmail.com>
2021-08-04 01:05:46 +0300

    Update calendar

--- ExamManagementWeb/src/app/calendar/calendar.component.ts
+++ ExamManagementWeb/src/app/calendar/calendar.component.ts
@@ -1,16 +1,74 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
-  styleUrls: ['./calendar.component.scss']
+  styleUrls: ['./calendar.component.scss'],
 })
 export class CalendarComponent implements OnInit {
+  ngOnInit(): void {}
 
-  constructor() { }
+  //selected = new Date();
 
-  ngOnInit(): void {
+  constructor(private renderer: Renderer2) {}
+  Today = [{ date: this.dateToString(new Date()), text: 'Today' }];
+
+  dates = [
+    { date: '2021-08-01', text: 'Special Day 1' },
+    { date: '2021-08-20', text: 'Special Day 2' },
+  ];
+  dateClass = (d: Date) => {
+    if (d.getDate() == 1) this.displayMonth();
+    const dateSearch = this.dateToString(d);
+    if (this.Today.find((f) => f.date == dateSearch)) {
+      return this.Today.find((f) => f.date == dateSearch)
+        ? 'todays_class'
+        : 'normal';
+    } else {
+      return this.dates.find((f) => f.date == dateSearch)
+        ? 'example-custom-date-class'
+        : 'normal';
+    }
+  };
+
+  displayMonth() {
+    setTimeout(() => {
+      let elements = document.querySelectorAll('.endDate');
+      console.log('*', elements.length);
+      let x = document.querySelectorAll('.mat-calendar-body-cell');
+      x.forEach((y) => {
+        const dateSearch = this.dateToString(
+          new Date(y.getAttribute('aria-label')!)
+        );
+        const data = this.dates.find((f) => f.date == dateSearch);
+        const data_today = this.Today.find((f) => f.date == dateSearch);
+        if (data) y.setAttribute('aria-label', data.text);
+        if (data_today) y.setAttribute('aria-label', data_today.text);
+      });
+    });
   }
-  selected = new Date();
-}
+  streamOpened() {
+    setTimeout(() => {
+      let buttons = document.querySelectorAll('mat-calendar .mat-icon-button');
 
+      buttons.forEach((btn) =>
+        this.renderer.listen(btn, 'click', () => {
+          setTimeout(() => {
+            //debugger
+            this.displayMonth();
+          });
+        })
+      );
+      this.displayMonth();
+    });
+  }
+  dateToString(date: any) {
+    return (
+      date.getFullYear() +
+      '-' +
+      ('0' + (date.getMonth() + 1)).slice(-2) +
+      '-' +
+      ('0' + date.getDate()).slice(-2)
+    );
+  }
+}
--- ExamManagementWeb/src/app/dashboard/dashboard.component.ts
+++ ExamManagementWeb/src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,80 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpResponse } from '@angular/common/http';
+import { Component, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AttendanceModel } from '../shared/models/attendance.model';
+import { ExamModel } from '../shared/models/exam.model';
+import { AttendanceService } from '../shared/services/attendance.service';
+import { ExamService } from '../shared/services/exam.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent implements OnInit {
-  constructor() {}
-  ngOnInit(): void {}
+export class DashboardComponent implements OnInit, OnDestroy {
+  @Output() public examsList: ExamModel[] = new Array<ExamModel>();
+  public upcomingExamsList: ExamModel[] = new Array<ExamModel>();
+  private subs: Subscription[];
+  public attendanceList!: AttendanceModel[];
+  userId: any;
+
+  constructor(
+    private readonly examService: ExamService,
+    private readonly attendanceService: AttendanceService
+  ) {
+    this.subs = new Array<Subscription>();
+  }
+
+  ngOnDestroy(): void {
+    this.subs.forEach((sub) => {
+      sub.unsubscribe();
+    });
+  }
+  ngOnInit(): void {
+    if (sessionStorage.getItem('identity') != null) {
+      this.userId = JSON.parse(sessionStorage.getItem('identity')!)['id'];
+
+      this.subs.push(
+        this.attendanceService
+          .getAllAttendance()
+          .subscribe((data: HttpResponse<any>) => {
+            this.attendanceList = data.body.filter(
+              (c: AttendanceModel) => c.studentId == this.userId
+            );
+
+            this.attendanceList.forEach((c: AttendanceModel) =>
+              this.examService
+                .getExam(c.examId)
+                .subscribe((data: HttpResponse<any>) => {
+                  this.examsList.push(data.body);
+                })
+            );
+
+            /*
+            this.upcomingExamsList = this.examsList.sort(function (
+              d1: ExamModel,
+              d2: ExamModel
+            ) {
+              const date1 = new Date(d1.date).getDay();
+              const date2 = new Date(d2.date).getDay();
+
+              console.log(date1);
+              console.log(date2);
+
+              if (date1 > date2) return 1;
+              if (date1 < date2) return -1;
+              return 0;
+            });
+
+            //console.log(this.upcomingExamsList);
+            console.log(this.examsList);
+            console.log(this.examsList[0]);
+            */
+          })
+
+      );
+    }
+  }
+
+  public getUpcomingExams() {}
 }
