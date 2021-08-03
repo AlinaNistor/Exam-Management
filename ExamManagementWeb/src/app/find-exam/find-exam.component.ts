c6c3af06df24c7ad0ea4418a02f9f0e036bc491e
Alina Nistor <alina.nistor99@gmail.com>
2021-08-04 01:06:36 +0300

    Implement search bar

--- ExamManagementWeb/src/app/find-exam/find-exam.component.ts
+++ ExamManagementWeb/src/app/find-exam/find-exam.component.ts
@@ -14,12 +14,17 @@ import { FacultyModel } from 'src/app/shared/models/faculty.model';
 })
 export class FindExamComponent implements OnInit, OnDestroy {
   public examsList: ExamModel[] = new Array<ExamModel>();
+  public filteredList: ExamModel[] = new Array<ExamModel>();
+
   public faculties: FacultyModel[] = new Array<FacultyModel>();
   private subs: Subscription[];
+  searchText: string = '';
 
-  constructor(private readonly examService: ExamService,
+  constructor(
+    private readonly examService: ExamService,
     private facultyService: FacultyService,
-    private router: Router) {
+    private router: Router
+  ) {
     this.subs = new Array<Subscription>();
   }
 
@@ -27,12 +32,13 @@ export class FindExamComponent implements OnInit, OnDestroy {
     this.subs.push(
       this.examService.getAllExams().subscribe((data: HttpResponse<any>) => {
         this.examsList = data.body;
+        this.filteredList = data.body;
 
         this.facultyService
-        .getFaculties()
-        .subscribe((faculties: HttpResponse<any>) => {
-          this.faculties = faculties.body;
-        });
+          .getFaculties()
+          .subscribe((faculties: HttpResponse<any>) => {
+            this.faculties = faculties.body;
+          });
       })
     );
   }
@@ -43,11 +49,27 @@ export class FindExamComponent implements OnInit, OnDestroy {
     });
   }
 
-  public getFacultyName(facultyId: string){
-    return this.faculties.find((c:FacultyModel) => c.id == facultyId)?.name;
+  public getFacultyName(facultyId: string) {
+    return this.faculties.find((c: FacultyModel) => c.id == facultyId)?.name;
   }
 
   goToExam(id: string): void {
     this.router.navigate([`/exam/details/${id}`]);
+  }
+
+  filter() {
+    var e = document.getElementById('searchType') as HTMLSelectElement;
+    var sel = e.selectedIndex;
+    var searchCriteria = e.options[sel].value;
+
+    if (searchCriteria == 'Title')
+      this.filteredList = this.examsList.filter((c: ExamModel) =>
+        c.name.toUpperCase().startsWith(this.searchText.toUpperCase())
+      );
+
+    if (searchCriteria == 'Teacher')
+      this.filteredList = this.examsList.filter((c: ExamModel) =>
+        c.headProfessor.toUpperCase().startsWith(this.searchText.toUpperCase())
+      );
   }
 }
