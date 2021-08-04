725aa86af80d76c20722357f68be8d6a28a64068
Alina Nistor <alina.nistor99@gmail.com>
2021-08-04 13:08:10 +0300

    Update dashboard

--- ExamManagementWeb/src/app/dashboard/dashboard.component.ts
+++ ExamManagementWeb/src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { HttpResponse } from '@angular/common/http';
 import { Component, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
 import { Subscription } from 'rxjs';
 import { AttendanceModel } from '../shared/models/attendance.model';
 import { ExamModel } from '../shared/models/exam.model';
@@ -20,7 +21,8 @@ export class DashboardComponent implements OnInit, OnDestroy {
 
   constructor(
     private readonly examService: ExamService,
-    private readonly attendanceService: AttendanceService
+    private readonly attendanceService: AttendanceService,
+    private readonly helper: JwtHelperService
   ) {
     this.subs = new Array<Subscription>();
   }
@@ -34,48 +36,63 @@ export class DashboardComponent implements OnInit, OnDestroy {
     if (sessionStorage.getItem('identity') != null) {
       this.userId = JSON.parse(sessionStorage.getItem('identity')!)['id'];
 
-      this.subs.push(
-        this.attendanceService
-          .getAllAttendance()
-          .subscribe((data: HttpResponse<any>) => {
-            this.attendanceList = data.body.filter(
-              (c: AttendanceModel) => c.studentId == this.userId
-            );
+      if (this.isAdmin()) {
+        this.examService.getAllExams().subscribe((data: HttpResponse<any>) => {
+          this.examsList = data.body.filter(
+            (c: ExamModel) => c.headProfessor == this.userId
+          );
+        });
+      } else {
+        this.subs.push(
+          this.attendanceService
+            .getAllAttendance()
+            .subscribe((data: HttpResponse<any>) => {
+              this.attendanceList = data.body.filter(
+                (c: AttendanceModel) => c.studentId == this.userId
+              );
 
-            this.attendanceList.forEach((c: AttendanceModel) =>
-              this.examService
-                .getExam(c.examId)
-                .subscribe((data: HttpResponse<any>) => {
-                  this.examsList.push(data.body);
-                })
-            );
+              this.attendanceList.forEach((c: AttendanceModel) =>
+                this.examService
+                  .getExam(c.examId)
+                  .subscribe((data: HttpResponse<any>) => {
+                    this.examsList.push(data.body);
+                  })
+              );
 
+              this.upcomingExamsList = this.examsList.sort(function (
+                d1: ExamModel,
+                d2: ExamModel
+              ) {
+                const date1 = new Date(d1.date).getDay();
+                const date2 = new Date(d2.date).getDay();
 
-            this.upcomingExamsList = this.examsList.sort(function (
-              d1: ExamModel,
-              d2: ExamModel
-            ) {
-              const date1 = new Date(d1.date).getDay();
-              const date2 = new Date(d2.date).getDay();
+                console.log(date1);
+                console.log(date2);
 
-              console.log(date1);
-              console.log(date2);
+                if (date1 > date2) return 1;
+                if (date1 < date2) return -1;
+                return 0;
+              });
 
-              if (date1 > date2) return 1;
-              if (date1 < date2) return -1;
-              return 0;
-            });
-
-            //console.log(this.upcomingExamsList);
-            console.log('test');
-            console.log(this.examsList);
-            console.log(this.examsList[0]);
-
-          })
-
-      );
+              //console.log(this.upcomingExamsList);
+              console.log('test');
+              console.log(this.examsList);
+              console.log(this.examsList[0]);
+            })
+        );
+      }
     }
   }
 
   public getUpcomingExams() {}
+
+  public isAdmin(): boolean {
+    var decodedToken = this.helper.decodeToken(
+      sessionStorage.getItem('userToken')!
+    );
+    if ('isAdmin' in decodedToken) {
+      return true;
+    }
+    return false;
+  }
 }
